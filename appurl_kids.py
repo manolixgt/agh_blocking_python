1798a894537bb50b1d90ce47b32c14b7552bc5f1
manolix <biggtronic@gmail.com>
2021-01-24 19:22:19 -0600

    py updated

--- appurl_kids.py
+++ appurl_kids.py
@@ -4,13 +4,13 @@ import asyncio
 # variable de nombre y url
 
 nuevaLista = "pc block"
-urlLista= ""
+urlLista= "https://raw.githubusercontent.com/manolixgt/agh_blocking_python/main/blocklists/pc_block.txt"
 
 
 async def main():
     
-    async with AdGuardHome("172.16.10.199") as adguard:
-        bloquear = await adguard.filtering.add_url(name = nuevaLista, url = urlLista)
+    async with AdGuardHome("172.16.10.199",password="lesli3,",port=80,username="admin") as adguard:
+        bloquear = await adguard.filtering.add_url(nuevaLista,urlLista)
         print("Nueva lista agregada", bloquear)
 
 if __name__ == "__main__":
