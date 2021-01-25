72e7d3f8113201da58037bde8268a54b1af62e30
manolix <biggtronic@gmail.com>
2021-01-24 19:40:37 -0600

    pc done

---
+++ deleteurl_pc.py
@@ -0,0 +1,19 @@
+from adguardhome import AdGuardHome
+import asyncio
+
+# variable de nombre y url
+
+nuevaLista = "pc block"
+urlLista= "https://raw.githubusercontent.com/manolixgt/agh_blocking_python/main/blocklists/pc_block.txt"
+
+
+async def main():
+    
+    async with AdGuardHome("172.16.10.199",password="lesli3,",port=80,username="admin") as adguard:
+        bloquear = await adguard.filtering.remove_url(urlLista)
+        print("Nueva lista eliminada", bloquear)
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    print()
